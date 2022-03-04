6463a7be533a739c3e3c684562083f3fa2347545
Chris <94694991+QCHR1581@users.noreply.github.com>
2022-03-04 13:31:03 +0100

    add local storage
    Now it is possible to create tasks and save them

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-var ul = document.querySelector("ul");
-var input = document.querySelector("#input-field");
-var button = document.querySelector("#input-button");
-var output = document.querySelector("#lsOutput");
-var storedInput = localStorage.getItem("textInput");
-
-var createListElement = () => {
+var createListElement = (txt) => {
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
-    input.value ="";
+    li.innerHTML = txt;
+
+    input.value="";
     var deleteButton = document.createElement("button");
     deleteButton.className = "fa fa-trash deletebtn";
     var doneButton = document.createElement("button");
     doneButton.className = "fa fa-check-square donebtn";
-    localStorage.setItem("textInput", input.value);
-
     li.appendChild(deleteButton);
     li.appendChild(doneButton);
-    deleteButton.onclick = function() {
-        this.parentElement.style.display = "none"
-    }
+  
+
+    deleteButton.addEventListener("click", (e) => {
+        li.parentNode.removeChild(li);
+        // this.parentElement.style.display = "none";
+        savedTasks = savedTasks.filter((e) => e !== txt); // remove the in-memory element
+        localStorage.setItem("tasks", JSON.stringify(savedTasks)); // store the new list in localStorage
+    })
+
     doneButton.onclick = function() {
         li.classList.toggle("done");
         this.classList.toggle("done");
     }
 }
+
+var ul = document.querySelector("ul");
+var input = document.querySelector("#input-field");
+var button = document.querySelector("#input-button");
+var list = document.querySelector("#list");
+
+// read previous tasks. If no tasks were found, start with an empty list
+let savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+// add UI elements for any saved task
+savedTasks.forEach(createListElement);
 
 var inputLength = () => {
 	return input.value.length;
@@ -32,14 +42,13 @@ var inputLength = () => {
 
 var addListAfterClick = () => {
 	if (inputLength() > 0) {
-		createListElement();
-	}
-}
-
-var addListAfterKeypress = (event) => {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+        let txt = input.value;
+        savedTasks.push(txt);
+        localStorage.setItem("tasks", JSON.stringify(savedTasks));
+        createListElement(txt);
+	} else {
+        alert("Please write something to do!");
+    }
 }
 
 var createDeleteButton = () => {
@@ -48,14 +57,15 @@ var createDeleteButton = () => {
 
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+        let txt = input.value;
+        savedTasks.push(txt);
+        localStorage.setItem("tasks", JSON.stringify(savedTasks));
+		createListElement(txt);
+	} else if (inputLength() === 0 && event.keyCode === 13)
+    alert("Please write something to do!")
 }
-
 
 button.addEventListener("click", addListAfterClick)
 
 input.addEventListener("keypress", addListAfterKeypress);
-
-
 
